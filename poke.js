666ada76819167f3b477eb9e7248c8b4f0ecfc72
Matheus Sales <mhss2904@gmail.com>
2023-08-10 17:14:56 -0300

    habilidades, peso e tipo agora são mostrados

--- poke.js
+++ poke.js
@@ -1,14 +1,42 @@
-const res = document.getElementById('res');
+var res = document.getElementById('res');
 
 async function buscar(){
     try {
         let pokeNameInput = document.getElementById('entra'); // Obtém o elemento input
         let pokeName = pokeNameInput.value.toLowerCase(); // Obtém o valor do input e converte para minúsculas
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`);
-        const data = await response.json();
-        const prettyData = JSON.stringify(data, null, 2);
-        res.innerHTML = `Resultado: ${prettyData}`;
+        var data = await response.json();
+        mostrarPokemon(data,pokeName);
+        //const prettyData = JSON.stringify(data, null, 2);
     } catch (error) {
         res.innerHTML = `Ocorreu um erro: ${error}`;
     }
+}
+
+function mostrarPokemon(jsonObj,pokeName){
+    res.innerHTML = '';
+    res.innerHTML += `Habilidades:<br>`;
+    
+    jsonObj.abilities.forEach(habilidade => {
+        var abilityName = habilidade.ability.name;
+        res.innerHTML += `${abilityName}<br>`;
+    });
+
+    let peso = Math.floor(jsonObj.weight/10);
+
+    let tipo = JSON.stringify(jsonObj.types[0].type.name).replace(/"/g,"");
+
+    res.innerHTML += `<br>Peso: ${peso} Kg`
+
+    res.innerHTML += `<br>Tipo: ${tipo}`
+
+    // res.innerHTML += `<br>Outras formas:<br>`;
+    // jsonObj.forms.forEach(forma => {
+    //     var formName = forma.name;
+    //     if(formName != pokeName){
+    //         res.innerHTML = `${formName}<br>`
+    //     } else {
+    //         res.innerHTML += `Não há outras formas`;
+    //     }
+    // });
 }
